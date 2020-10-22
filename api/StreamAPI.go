208bbe6ad316036cf9205a2696ccbfdb48b1cc30
Xavier Tordoir <xtordoir@yahoo.co.uk>
2020-10-22 10:25:21 +0200

    Recover from a panic in ClientStream and autoRestart for use in TickStream (WIP)

--- api/StreamAPI.go
+++ api/StreamAPI.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/xtordoir/goanda/models"
 )
@@ -19,15 +20,30 @@ type StreamAPI struct {
 type priceProcessor func(p *models.ClientPrice)
 type heartbeatProcessor func(p *models.PricingHeartbeat)
 
-// TickStream starts a stream of ticks, hiding the Prices structs
+// TickStream starts a stream of ticks, hiding the Prices structs which are autoRestarted
 func (streamApi *StreamAPI) TickStream(instruments []string, tchan chan models.Tick, hchan chan models.PricingHeartbeat) {
 	pchan := make(chan models.ClientPrice)
-	go streamApi.PricingStream(instruments, pchan, hchan)
+	// AutoRestart for PricingStream
+	go autoRestart("PricingStream", 0, func() { streamApi.PricingStream(instruments, pchan, hchan) })
+
 	fmt.Println("Starting loop on Prices")
 	for {
 		price := <-pchan
 		tchan <- models.ClientPrice2Tick(&price)
 	}
+}
+
+// AutoRestart for the PricingStream function as connection reset can result in panic
+func autoRestart(name string, nPanics int64, f func()) {
+	defer func() {
+		if v := recover(); v != nil {
+			// A panic is detected.
+			log.Printf("%s is crashed. Panic #%d. Restarting in 5 seconds.", name, nPanics+1)
+			time.Sleep(5 * time.Second)
+			go autoRestart(name, nPanics+1, f) // restart
+		}
+	}()
+	f()
 }
 
 // PricingStream starts a stream of prices
@@ -47,7 +63,7 @@ func (streamApi *StreamAPI) PricingStream(instruments []string, pchan chan model
 			line, err := reader.ReadBytes('\n')
 			if err != nil {
 				log.Println(err)
-				break
+				panic("Connection on clientStream is lost")
 			}
 			var p models.ClientPrice
 			json.Unmarshal([]byte(line), &p)
